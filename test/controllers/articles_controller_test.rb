84b5ece1edfe1dc3bd487c201ddc04ccd2276fbb
HanZhou <1400727642@qq.com>
2022-04-15 00:16:30 -0700

    add test case, modify fixtures/article

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -19,7 +19,7 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
 
   test 'should create article' do
     assert_difference('Article.count') do
-      post articles_url, params: { article: { description: @article.description, title: @article.title } }
+      post articles_url, params: { article: { description: @article.description, title: @article.title, author: @article.author } }
     end
 
     assert_redirected_to article_url(Article.last)
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -6,4 +6,9 @@ class ArticleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'Article without title should not saved' do
+    article = Article.new
+    assert !article.save
+  end
 end
